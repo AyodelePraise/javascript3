b1b8a5f38fc254586da97ab409e67ef690fd0e82
Praise <aypraise19@gmail.com>
2023-02-07 18:05:58 +0100

    javascript3

---
+++ app.js
@@ -0,0 +1,35 @@
+var arr = [[1,2,3],[4,5,6],7,8,9,[[10,11,12,[13,4,112]],136,14],];
+
+console.log(arr[5][0][0]);
+
+
+console.log(arr[3]);
+
+console.log(arr[5][0][3][0]);
+
+console.log(arr[5][1]);
+
+console.log(arr[1][0])
+
+
+
+
+//This is Question 2
+
+var sentence = ["javascript", "web", "CapacityBay"];
+  console.log("I am a", sentence[1], "Developer at",  sentence[2], "and I love", sentence[0])
+
+
+  //This is question 3
+  let word = ["G", "E", "N", "E", "R", "A", "L", "I", "Z", "A", "T", "I", "O", "N"];
+    console.log(word[10]);
+
+//This is question 4
+
+    let fruits = ["orange", "apple", "guava", "lemon"];
+
+    fruits.unshift("pea", "banana")
+
+    fruits.push("Avocado", "Mango")
+
+    console.log(fruits)
